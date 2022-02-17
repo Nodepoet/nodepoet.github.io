7fddeba71658ca07d77049dbe2f05cee4da5cf44
Nodepoet <98703006+Nodepoet@users.noreply.github.com>
2022-02-16 21:48:41 -0500

    Add files via upload

---
+++ public/scripts/index.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.validated')
+
+    // Loop over them and prevent submission
+    Array.from(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
+
+function flip(event){
+	var element = event.currentTarget;
+	if (element.className === "flipper") {
+    if(element.style.transform == "rotateY(180deg)") {
+      element.style.transform = "rotateY(0deg)";
+    }
+    else {
+      element.style.transform = "rotateY(180deg)";
+    }
+  }
+};
